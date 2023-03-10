a509cde509560ae32a721e6fc863a0c31733e894
JeanxLiao <88898152+JackieesLiao@users.noreply.github.com>
2023-03-10 12:58:29 +0800

    Add files via upload

---
+++ Primer_CH05/ex5_9.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	char ch;
+	int o_count=0;
+	cout << "输入一段文本：";
+	while(cin >> ch)
+	{
+      if (ch == 'a')
+        ++o_count;
+	  if (ch == 'e')
+	    ++o_count;
+      if (ch == 'u')
+		++o_count;
+	  if (ch == 'i')
+		++o_count;
+	  if (ch == 'o')
+		++o_count;
+	 
+	}
+	cout << "共有" << o_count << "个元音字母。" << endl;
+	
+}
