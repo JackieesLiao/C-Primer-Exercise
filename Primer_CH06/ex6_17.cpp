9752d91f66dc23e20cc4bfbd5f2fe904219c91a8
JeanxLiao <88898152+JackieesLiao@users.noreply.github.com>
2023-03-17 22:13:48 +0800

    Update ex6_17.cpp

--- Primer_CH06/ex6_17.cpp
+++ Primer_CH06/ex6_17.cpp
@@ -1,1 +1,29 @@
+ include<iostream>
+ #include<cctype>
+ #include<string>
+ bool judgeUpperWord(const std::string&str)
+ {
+     for(auto ch:str)
+     {
+         if(isupper(ch))
+             return true;
+     }
+     return false;
+ }
+
+ void  changeStringToLower(std::string &str)
+ {
+     for(auto &ch:str)
+        ch = tolower(ch);
+ }
+ int main()
+ {
+   std::string str1="LINUX are a incredible OS.";
+   bool rex=judgeUpperWord(str1);
+   std::cout<<" If rex=1 means there are right:"<<rex<<std::endl;
+   std::string str2="HUNAN PROVINCE ,i love u";
+   changeStringToLower(str2);
+   std::cout<<"After changed."<<str2<<std::endl;
+   return 0;
+ }
 
