a8160ef23ffd751085c1d63dc54984f4e0b37c2c
JeanxLiao <88898152+JackieesLiao@users.noreply.github.com>
2023-03-18 14:51:05 +0800

    Update ex6_30.cpp

--- Primer_CH06/ex6_30.cpp
+++ Primer_CH06/ex6_30.cpp
@@ -16,3 +16,10 @@ int main()
     bool result=str_subrange(str1,str2);    
     std::cout<<result<<std::endl;    
 }  
+/*
+总共有两个错误，一个无返回值，另一个是控制流尚未返回任何值就结束函数的执行。后者编译器检查不出。
+ex6_30.cpp: In function ‘bool str_subrange(const string&, const string&)’:
+ex6_30.cpp:10:13: error: return-statement with no value, in function returning ‘bool’ [-fpermissive]
+   10 |             return ;//error
+      |      
+*/
